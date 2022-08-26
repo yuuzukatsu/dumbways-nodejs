228a94eb881c08622c0bb6b0115052274574a311
Yuuzukatsu <67664879+yuuzukatsu@users.noreply.github.com>
2022-08-26 15:49:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!This is Node.JS App!");
+  res.send("Hello Dumbways!This is Node.JS App!");
 });
 
 app.listen(port, () => {
